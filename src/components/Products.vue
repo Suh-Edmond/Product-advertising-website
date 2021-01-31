f0f85c459b66c227c54f3e0fbd8d5bce23db4895
Suh Edmond <suhedmond25@yahoo.com>
2021-01-31 18:31:35 +0100

    Setup Vuex store for products

---
+++ src/components/Products.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page>
+    <div v-for="product in products" :key="product.id">
+      <p>{{ product.product_name }}</p>
+      <p>{{ product.price }}</p>
+      <p>{{ product.quantity }}</p>
+      <p>{{ product.descripiton }}</p>
+      <p>{{ product.condition }}</p>
+      <p>{{ product.discount }}</p>
+      <p>{{ product.service }}</p>
+      <p>{{ product.in_stocked }}</p>
+      <p>{{ product.published }}</p>
+    </div>
+    <q-btn color="white" text-color="black" label="Standard" @click="work" />
+  </q-page>
+</template>
+
+<script>
+export default {
+  props: ["products"],
+
+  methods: {
+    work() {}
+  }
+};
+</script>
+
+<style></style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,29 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-// import example from './module-example'
+// we first import the module
+import products from './store-products'
 
 Vue.use(Vuex)
-
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Store instance.
- */
 
 export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
     modules: {
-      // example
+      // then we reference it
+      products
     },
 
     // enable strict mode (adds overhead!)
     // for dev mode only
-    strict: process.env.DEBUGGING
+    strict: process.env.DEV
   })
+
+  /*
+    if we want some HMR magic for it, we handle
+    the hot update like below. Notice we guard this
+    code with "process.env.DEV" -- so this doesn't
+    get into our production build (and it shouldn't).
+  */
+
+ 
 
   return Store
 }
